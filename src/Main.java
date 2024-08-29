b2a00c3e0ac2018c28876e21ea0efbcecc03e4c2
Jocund96 <jasurbek.odilov@gmail.com>
2024-08-29 15:03:28 +0500

    1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        //TIP Java First Tasks
+        System.out.printf("Hello my friend welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        //TIP Uncomment task number to see the result
+        first();
+        second();
+        third();
+        fourth();
+        fifth();
+        sixth();
+        seventh();
+
+
+        }
+    static void first() {
+        System.out.println("Hello World!");
+    }
+    static void second() {
+        System.out.println("Hello World!");
+    }
+    static void third() {
+        System.out.println("Hello World!");
+    }
+    static void fourth() {
+        System.out.println("Hello World!");
+    }
+    static void fifth() {
+        System.out.println("Hello World!");
+    }
+    static void sixth() {
+        System.out.println("Hello World!");
+    }
+    static void seventh() {
+        System.out.println("Hello World!");
+    }
+    }
