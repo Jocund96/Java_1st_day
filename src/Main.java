389dc13a13287d2065653d515ba239112a69fafd
Jocund96 <jasurbek.odilov@gmail.com>
2024-08-29 15:56:10 +0500

    1st task

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,49 @@
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Java First Tasks
-        System.out.printf("Hello my friend welcome!");
+        //TIP Java First Tasks by Jocund
+        System.out.println("Hello and welcome my friend !\n");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-        //TIP Uncomment task number to see the result
-        first();
+
+        //Uncomment task number to see the result to see the result
+
+        //first();
         second();
-        third();
-        fourth();
-        fifth();
-        sixth();
-        seventh();
-
-
+        //third();
+        //fourth();
+        //fifth();
+        //sixth();
+        //seventh();
         }
     static void first() {
-        System.out.println("Hello World!");
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("___");
+        }
+        System.out.print("\n");
+        System.out.println("| Mr.Jasurbek\t\t\t\t\t\t\t\t\t\t\t   |");
+        System.out.println("| 25 Shakhrisabz street\t\t\t\t\t\t\t\t\t   |");
+        System.out.println("| Tashkent city, 100000\t\t\t\t\t\t\t\t\t   |");
+        System.out.println("| UZ \t\t\t\t\t\t\t\t\t\t\t\t\t   |");
+        System.out.print("|");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("\t\t");
+        }
+        System.out.println("Mr. Mukhammadali Khayotov \t\t   |");
+        System.out.print("|");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("\t\t");
+        }
+        System.out.println("209 Neungdong-ro, Seoul \t\t   |");
+        System.out.print("|");
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("\t\t");
+        }
+        System.out.println("Republic of Korea\t\t\t\t   |");
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("___");
+        }
+
     }
     static void second() {
         System.out.println("Hello World!");
